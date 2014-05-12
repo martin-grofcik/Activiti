f75f2b7b86da92c700c19590246a6cfd835d4b38
martin.grofcik <martin.grofcik@edorasware.com>
2014-05-12 12:49:55 +0200

    TASK_CREATED event

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/UserTaskActivityBehavior.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/UserTaskActivityBehavior.java
@@ -16,6 +16,8 @@ import org.activiti.engine.ActivitiException;
 import org.activiti.engine.ActivitiIllegalArgumentException;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.delegate.event.ActivitiEventType;
+import org.activiti.engine.delegate.event.impl.ActivitiEventBuilder;
 import org.activiti.engine.impl.calendar.BusinessCalendar;
 import org.activiti.engine.impl.calendar.DueDateBusinessCalendar;
 import org.activiti.engine.impl.context.Context;
@@ -107,8 +109,14 @@ public class UserTaskActivityBehavior extends TaskActivityBehavior {
     
     handleAssignments(task, execution);
    
-    // All properties set, now firing 'create' event
+    // All properties set, now firing 'create' events
+    if (Context.getProcessEngineConfiguration().getEventDispatcher().isEnabled()) {
+      Context.getProcessEngineConfiguration().getEventDispatcher().dispatchEvent(
+        ActivitiEventBuilder.createEntityEvent(ActivitiEventType.TASK_CREATED, task));
+    }
+
     task.fireEvent(TaskListener.EVENTNAME_CREATE);
+
   }
 
   public void signal(ActivityExecution execution, String signalName, Object signalData) throws Exception {
